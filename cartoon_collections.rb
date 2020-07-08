3d1443903d443f2a0d9859193118c8762f500ec2
Chindalath Traymany <chindalatht@gmail.com>
2020-07-08 10:41:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
   list.find do |ingredient|
-    cheese_types[ingredient]
+    cheese_types[1...-1]
   end
 end
 
