e31ecdbea28bdc1c431bb9facc90b317a0e7d22b
Chindalath Traymany <chindalatht@gmail.com>
2020-07-08 10:30:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  array.map do |element|
+  planeteer_calls.map do |element|
     element.capitalize << "!"
   end
 end
